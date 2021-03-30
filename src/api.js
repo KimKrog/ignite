a8d47a4e56b7b710e62539a042e9fad51d6d858d
Kim Krog <kimkrog@Kims-MacBook-Pro.local>
2021-03-30 23:09:19 +0200

    Setup reducers

--- src/api.js
+++ src/api.js
@@ -1,33 +1,33 @@
-const base_url = 'https://api.rawg.io/api/';
+const base_url = "https://api.rawg.io/api/";
 
 // Getting the date
 const getCurrentMonth = () => {
-    const month = new Date().getMonth() + 1;
+  const month = new Date().getMonth() + 1;
 
-    if (month < 10) {
-        return `0${month}`;
-    } else {
-        return month;
-    }
-}
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
 
 // Getting the day
 const getCurrentDay = () => {
-    const day = new Date().getDate() + 1;
+  const day = new Date().getDate();
 
-    if (day < 10) {
-        return `0${day}`;
-    } else {
-        return day;
-    }
-}
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
 
-const currentYear  = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
-const currentDay   = getCurrentDay();
-const currentDate  = `${currentYear}-${currentMonth}-${currentDay}`;
-const lastYear     = `${currentYear - 1}-${currentMonth}-${currentDay}`;
-const nextYear     = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import App from "./App";
+import rootReducer from "./reducers";
+import reportWebVitals from "./reportWebVitals";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
