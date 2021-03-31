9255ca6b93a16ce80d85f55807a9d95aea0959a1
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-03-31 21:34:40 +0200

    Output data + global styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,77 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+
+      <h2>Popular Games</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
+          <Game
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
 
 export default Home;
+
+const GameList = styled(motion.div)`
+  padding: 0 5rem;
+
+  h2 {
+    padding: 5rem 0;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 70vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
